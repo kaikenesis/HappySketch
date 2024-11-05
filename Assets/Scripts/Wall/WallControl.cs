7cf2b43add360bcee9c2ec504d0d13ded7cb1414
skytower144 <skytower144@naver.com>
2024-11-06 02:48:04 +0900

    chore: 벽 활성화/비활성화 기능 작업 중

---
+++ Assets/Scripts/Wall/WallControl.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallControl : MonoBehaviour
+{
+    [SerializeField] private Transform player1Transform, player2Transform;
+    [SerializeField] private List<Transform> walls;
+    [SerializeField] private MeshRenderer meshRenderer;
+    private float wallHeight;
+
+    void Start()
+    {
+        wallHeight = meshRenderer.bounds.size.y;
+        walls[2].gameObject.SetActive(false);
+        walls[3].gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+
+    }
+}
