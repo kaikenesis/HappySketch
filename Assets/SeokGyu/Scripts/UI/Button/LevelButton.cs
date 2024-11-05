fc894e3144c3f1668382f8461527d381592b7da2
kenesis <78710219+kaikenesis@users.noreply.github.com>
2024-11-05 17:57:21 +0900

    작업 목록 : Cartoon GUI 다운, LevelButton 작업

--- Assets/SeokGyu/Scripts/UI/Button/LevelButton.cs
+++ Assets/SeokGyu/Scripts/UI/Button/LevelButton.cs
@@ -1,6 +1,15 @@
+using System;
+using TMPro;
+using UnityEngine;
 
 public class LevelButton : BaseButton
 {
+    [SerializeField]
+    private ELevel level;
+    private TextMeshProUGUI textName;
+    [SerializeField]
+    private GameObject starImg;
+
     private void Awake()
     {
         Init();
@@ -14,6 +23,11 @@ public class LevelButton : BaseButton
     protected override void Init()
     {
         base.Init();
+
+        textName = GetComponentInChildren<TextMeshProUGUI>();
+
+        SetLevelName();
+        SetStarCount();
     }
 
     protected override void SetInfo()
@@ -25,4 +39,37 @@ public class LevelButton : BaseButton
     {
         // 키설명 화면으로 전환
     }
+
+    private void SetLevelName()
+    {
+        switch (level)
+        {
+            case ELevel.Easy:
+                textName.SetText("Easy");
+                break;
+            case ELevel.Normal:
+                textName.SetText("Normal");
+                break;
+            case ELevel.Hard:
+                textName.SetText("Hard");
+                break;
+        }
+    }
+
+    private void SetStarCount()
+    {
+        for (int i = 0; i < (int)level; i++)
+        {
+            GameObject ob = Instantiate(starImg);
+            ob.transform.SetParent(transform);
+            ob.transform.position = starImg.transform.position;
+        }
+    }
+}
+
+enum ELevel
+{
+    Easy,
+    Normal,
+    Hard
 }
