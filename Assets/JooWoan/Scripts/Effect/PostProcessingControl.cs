de6853a1c656b91c6b476fa6954199410236283c
skytower144 <skytower144@naver.com>
2024-11-17 02:04:14 +0900

    feat: Fever Time 화면 테두리 효과 추가

---
+++ Assets/JooWoan/Scripts/Effect/PostProcessingControl.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class PostProcessingControl : MonoBehaviour
+{
+    private PostProcessVolume processVolume;
+    private Animator anim;
+    private Vignette vignette;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        processVolume = GetComponent<PostProcessVolume>();
+        processVolume.profile.TryGetSettings<Vignette>(out vignette);
+
+        if (!vignette)
+        {
+            Debug.LogWarning("Vignette is not properly initialized");
+            return;
+        }
+        vignette.enabled.Override(false);
+
+        if (!anim)
+            Debug.LogWarning("Post processing animator is not properly initialized");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            PlayFeverEffect();
+
+        else if (Input.GetKeyDown(KeyCode.Backspace))
+            StopFeverEffect();
+    }
+
+    public void PlayFeverEffect()
+    {
+        vignette.enabled.Override(true);
+        anim.Play("FeverEffect", -1, 0f);
+    }
+
+    public void StopFeverEffect()
+    {
+        vignette.enabled.Override(false);
+        anim.Play("PostProcessing_default", -1, 0f);
+    }
+}
