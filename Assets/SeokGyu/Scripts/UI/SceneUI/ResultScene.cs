f02c16f2890274e0e2eff2f125f111acc3551d21
kenesis <78710219+kaikenesis@users.noreply.github.com>
2024-11-18 17:49:02 +0900

    feat : 결과에 따른 승리, 패배 backImg 적용 배치 및, 색상 변경기능 추가 TimeProgress 색상 or 이미지 변경 함수만 추가( 기능 없음 )

--- Assets/SeokGyu/Scripts/UI/SceneUI/ResultScene.cs
+++ Assets/SeokGyu/Scripts/UI/SceneUI/ResultScene.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ResultScene : BaseScene
 {
     [SerializeField] private GameObject resultScoreObject;
+    [SerializeField] private GameObject backImgObject;
     [SerializeField] private float distance = 500;
     private ResultScore[] resultScores;
+    private Image[] backImgs;
 
     private void Start()
     {
@@ -15,14 +18,30 @@ public class ResultScene : BaseScene
     {
         int playerNum = UIManager.Instance.playerNum;
         resultScores = new ResultScore[playerNum];
+        backImgs = new Image[playerNum];
+        float backImgSizeX = Screen.width / playerNum;
+        float backImgPosX = backImgSizeX * -0.5f;
 
         for (int i = 0; i < playerNum; i++)
         {
             distance *= -1;
-            GameObject gameObject = Instantiate(resultScoreObject);
-            gameObject.transform.SetParent(transform);
-            gameObject.transform.localPosition = new Vector3(distance, 0, 0);
-            resultScores[i] = gameObject.GetComponent<ResultScore>();
+            GameObject resultScore = Instantiate(resultScoreObject);
+            resultScore.transform.SetParent(transform);
+            resultScore.transform.localPosition = new Vector3(distance, 0, 0);
+            resultScore.transform.SetAsFirstSibling();
+            resultScores[i] = resultScore.GetComponent<ResultScore>();
+        }
+
+        for (int i = 0; i < playerNum; i++)
+        {
+            GameObject backImg = Instantiate(backImgObject);
+            backImg.transform.SetParent(transform);
+            RectTransform backImgRect = backImg.GetComponent<RectTransform>();
+            backImgRect.sizeDelta = new Vector2(backImgSizeX, Screen.height);
+            backImg.transform.localPosition = new Vector3(backImgPosX, 0, 0);
+            backImg.transform.SetAsFirstSibling();
+            backImgPosX += backImgSizeX;
+            backImgs[i] = backImg.GetComponent<Image>();
         }
     }
 
@@ -41,9 +60,15 @@ public class ResultScene : BaseScene
         for (int i = 0; i < UIManager.Instance.playerNum; i++)
         {
             if(i != playerNum)
+            {
                 resultScores[i].SetText(false, i);
+                backImgs[i].color = new Color(0, 0, 0, 0.25f);
+            }
             else
+            {
                 resultScores[i].SetText(true, i);
+                backImgs[i].color = new Color(1, 1, 0, 0.25f);
+            }
         }
     }
 }
--- Assets/SeokGyu/Scripts/UI/Text/TimeProgress.cs
+++ Assets/SeokGyu/Scripts/UI/Text/TimeProgress.cs
@@ -1,13 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimeProgress : MonoBehaviour
 {
     [SerializeField] private GameObject textObject;
     private TextMeshProUGUI timeText;
-    
+    private Image progressImg;
 
     private void Awake()
     {
@@ -18,6 +17,7 @@ public class TimeProgress : MonoBehaviour
     {
         timeText = textObject.GetComponentInChildren<TextMeshProUGUI>();
         timeText.text = UIManager.Instance.maxTime.ToString() + "초";
+        progressImg = GetComponent<Image>();
     }
 
     public void SetText()
@@ -28,4 +28,8 @@ public class TimeProgress : MonoBehaviour
         timeText.text = UIManager.Instance.curTime.ToString() + "초";
     }
 
+    public void SetProgressColor()
+    {
+        // 피버타임때 무지개색으로 변경
+    }
 }
